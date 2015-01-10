d9008c8c90392ebfd101a9e8654d16979427ef4e
gateacom <jussi@loop8.fi>
2015-01-10 21:01:53 +0200

    Initial commit

---
+++ L8ActionColumn.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @copyright Copyright (c) 2015 Loop 8 ay
+ * @link http://loop8.fi
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace loop8\l8actioncolumn;
+
+use Yii;
+use Closure;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/**
+ * Extends the YII2 Action Column
+ */
+class L8ActionColumn extends \yii\grid\ActionColumn
+{
+    /**
+     * Return the default view button
+     */
+    public static function viewButton($url, $model, $key, $visible)
+    {
+        if (!$visible)
+            return '';
+
+        return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
+            'title' => Yii::t('yii', 'View'),
+            'data-pjax' => '0',
+        ]);
+    }
+
+    /**
+     * Return the default update button
+     */
+    public static function updateButton($url, $model, $key, $visible)
+    {
+        if (!$visible)
+            return '';
+
+        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+            'title' => Yii::t('yii', 'Update'),
+            'data-pjax' => '0',
+        ]);
+    }
+
+    /**
+     * Return the default delete button
+     */
+    public static function deleteButton($url, $model, $key, $visible)
+    {
+        if (!$visible)
+            return '';
+
+        return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+            'title' => Yii::t('yii', 'Delete'),
+            'data-pjax' => '0',
+        ]);
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+Extended ActionColumn for YII2 GridView
+=======================================
+Adds functions to get default button markup
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist loop8/yii2-l8-actioncolumn "*"
+```
+
+or add
+
+```
+"loop8/yii2-l8-actioncolumn": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \loop8\l8actioncolumn\AutoloadExample::widget(); ?>```
