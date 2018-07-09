9b72cf9396667c785de66a6d6984053f83fd5063
Stefan Guse <stefan@loop8.fi>
2018-07-09 23:49:50 +0300

    Update L8ActionColumn.php

--- L8ActionColumn.php
+++ L8ActionColumn.php
@@ -8,17 +8,21 @@
 namespace loop8\l8actioncolumn;
 
 use Yii;
-//use Closure;
+use yii\grid\ActionColumn;
 use yii\helpers\Html;
-//use yii\helpers\Url;
 
 /**
  * Extends the YII2 Action Column
  */
-class L8ActionColumn extends \yii\grid\ActionColumn
+class L8ActionColumn extends ActionColumn
 {
     /**
      * Return the default view button
+     * @param $url
+     * @param $model
+     * @param $key
+     * @param bool $visible
+     * @return string
      */
     public static function viewButton($url, $model, $key, $visible = true)
     {
@@ -33,6 +37,11 @@ class L8ActionColumn extends \yii\grid\ActionColumn
 
     /**
      * Return the default update button
+     * @param $url
+     * @param $model
+     * @param $key
+     * @param bool $visible
+     * @return string
      */
     public static function updateButton($url, $model, $key, $visible = true)
     {
@@ -47,6 +56,11 @@ class L8ActionColumn extends \yii\grid\ActionColumn
 
     /**
      * Return the default delete button
+     * @param $url
+     * @param $model
+     * @param $key
+     * @param bool $visible
+     * @return string
      */
     public static function deleteButton($url, $model, $key, $visible = true)
     {
@@ -61,12 +75,17 @@ class L8ActionColumn extends \yii\grid\ActionColumn
 
     /**
      * Return the default delete button for ajax
+     * @param $url
+     * @param $model
+     * @param $key
+     * @param bool $visible
+     * @param array $additionalOptions
+     * @return string
      */
     public static function ajaxDeleteButton($url, $model, $key, $visible = true, $additionalOptions = [])
     {
         if (!$visible)
             return '';
-
         $options = [
             'title' => Yii::t('yii', 'Delete'),
             'data-url' => $url,
@@ -78,5 +97,57 @@ class L8ActionColumn extends \yii\grid\ActionColumn
         }
 
         return Html::a('<span class="glyphicon glyphicon-trash"></span>', '#', $options);
+    }
+
+    /**
+     * Return the default approve button for ajax
+     * @param $url
+     * @param $model
+     * @param $key
+     * @param bool $visible
+     * @param array $additionalOptions
+     * @return string
+     */
+    public static function ajaxApproveButton($url, $model, $key, $visible = true, $additionalOptions = [])
+    {
+        if (!$visible)
+            return '';
+        $options = [
+            'title' => Yii::t('yii', 'Approve'),
+            'data-url' => $url,
+            'class' => 'l8ajax-approve',
+        ];
+
+        if (!empty($additionalOptions)) {
+            $options = array_merge($options, $additionalOptions);
+        }
+
+        return Html::a('<span class="glyphicon glyphicon-thumbs-up"></span>', '#', $options);
+    }
+
+    /**
+     * Return the default revoke approval button for ajax
+     * @param $url
+     * @param $model
+     * @param $key
+     * @param bool $visible
+     * @param array $additionalOptions
+     * @return string
+     */
+    public static function ajaxRevokeApprovalButton($url, $model, $key, $visible = true, $additionalOptions = [])
+    {
+        if (!$visible)
+            return '';
+        $options = [
+            'title' => Yii::t('yii', 'Revoke approval'),
+            'data-url' => $url,
+            'class' => 'l8ajax-revoke-approval',
+        ];
+
+        if (!empty($additionalOptions)) {
+            $options = array_merge($options, $additionalOptions);
+        }
+
+        return Html::a('<span class="glyphicon glyphicon-thumbs-down"></span>', '#', $options);
     }
 }
